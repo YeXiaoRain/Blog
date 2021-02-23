d5aa52b5e2149acfb86ded9a2dd358406aadd363
cromarmot <yexiaorain@gmail.com>
2021-02-23 15:53:58 +0800

    update by script

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
-git add . && git commit -m "update" && git push origin master
+git add . && git commit -m "update by script" && git push origin master
 # hexo generate && cd public && git add . && git commit -m "update" && git push origin gh-pages && cd ../
