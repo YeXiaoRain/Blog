d278c3367a1620da7b750c7a619a3d2fcbc5c46c
cromarmot <yexiaorain@gmail.com>
2022-09-02 11:53:21 +0800

    update & commit by script

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env sh
+
 git add . && git commit -m "update & commit by script" && git push origin master
 # rm -rf public && hexo g && cd public && git init && git checkout -b gh-pages && git add . && git commit -m "update" && git remote add origin git@github.com:CroMarmot/Blog.git && git push origin gh-pages -f && cd ../
