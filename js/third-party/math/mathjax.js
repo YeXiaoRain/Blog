b99df5dde80973ad4fe9d9cd2eedd214a7bb9fd1
CroMarmot <yexiaorain@gmail.com>
2022-09-02 13:04:37 +0000

    CroMarmot published a site update

--- js/third-party/math/mathjax.js
+++ js/third-party/math/mathjax.js
@@ -16,6 +16,8 @@ document.addEventListener('page:loaded', () => {
               const target = node.parentNode;
               if (target.nodeName.toLowerCase() === 'li') {
                 target.parentNode.classList.add('has-jax');
+              }else{
+                target.classList.add('has-jax');
               }
             });
           }, '', false]
